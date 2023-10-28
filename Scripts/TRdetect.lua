5f4aa697369c14a116ed4dd409a6b14377883677
Dave Corley <smokydave@icloud.com>
2023-10-28 17:50:26 -0500

    Feat(Scripts): Add short script to check for TR

--- Scripts/TRdetect.lua
+++ Scripts/TRdetect.lua
@@ -2,20 +2,15 @@ local core = require("openmw.core")
 local world = require("openmw.world")
 
 local function checkTR(player)
+  local globals = world.mwscript.getGlobalVariables(player)
   if core.contentFiles.has("TR_Mainland.esm") then
-    local globals = world.mwscript.getGlobalVariables(player)
-    globals.hasTR = 1
+    globals.AAV_hasTR = 1
+  else
+    globals.AAV_hasTR = 0
   end
 end
 
-
-
 return {
-    interfaceName = 'AAV_global',
-    interface = {
-      hasTR = hasTR,
-      version = 001,
-    },
     engineHandlers = {
       onPlayerAdded = checkTR
     }
