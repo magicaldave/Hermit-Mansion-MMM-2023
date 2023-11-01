30dad3a9d6f3733779ee9cd1031dcf43f56ae826
Dave Corley <smokydave@icloud.com>
2023-11-01 03:58:59 -0500

    Cleanup(TransmuteCube): Alphabetize require statements and make adjustments for AAV-specific behaviors

--- Scripts/global_transmuteCube.lua
+++ Scripts/global_transmuteCube.lua
@@ -1,12 +1,11 @@
-local core = require('openmw.core')
-local vfs = require('openmw.vfs')
-local util = require('openmw.util')
-local types = require('openmw.types')
 local async = require('openmw.async')
-local world = require('openmw.world')
-local storage = require('openmw.storage')
 local aux_util = require('openmw_aux.util')
-
+local core = require('openmw.core')
+local storage = require('openmw.storage')
+local types = require('openmw.types')
+local util = require('openmw.util')
+local vfs = require('openmw.vfs')
+local world = require('openmw.world')
 
 
 --affecting bug: https://gitlab.com/OpenMW/openmw/-/issues/7663
@@ -23,18 +22,18 @@ local startWithCube = true
 local startWithRecipes = true
 
 local theCube = nil
-local cubeChest = nil
+local cubeChest = world.getObjectByFormId(core.getFormId('the_Arcane_Academy_of_Venarius_School_Alpha_02.esm', 7004))
 local theBook = nil
 
 
 
 --TODO: update this to use player for multiplayer when the time comes
 --we cant just keep a reference, because the object is ""deleted"" when we teleport it to another cell.
-local function getCubeChest(player)
+local function getCubeChest()
 	return cubeChest
 end
 
-local function getRecipeBook(player)
+local function getRecipeBook()
 	return theBook
 end
 
@@ -44,11 +43,11 @@ local function onPlayerAdded(player)
 end
 
 local function getCubeContents(player)
-	return types.Container.inventory(getCubeChest(player)):getAll()
+	return types.Container.inventory(getCubeChest()):getAll()
 end
 
 --update to get the cube specefic to the player, when multiplayer becomes a thing
-local function getCube(player)
+local function getCube()
 	return theCube
 end
 
@@ -57,7 +56,7 @@ local function hasCube(player)
 	local inv = types.Actor.inventory(player):getAll(types.Miscellaneous)
 
 	for _, item in pairs(inv) do
-		if item.recordId == "misc_dwrv_ark_cube00" then
+		if item.recordId == "aav_transmutecube" then
 			return true
 		end
 	end
@@ -330,39 +329,39 @@ local function e_rx_hadCube(data)
 	if data.hadCube == true then return end
 	print("--did not have cube. continuing")
 
-	cubeChest = world.createObject("dwemer_cube", 1)
-	theCube = world.createObject("misc_dwrv_ark_cube00", 1)
+	theCube = world.createObject("aav_transmutecube", 1)
 	theBook = world.createObject("bk_dwemerCubeRecipes", 1)
+	theBook:moveInto(types.Actor.inventory(data.player))
+	theCube:moveInto(types.Actor.inventory(data.player))
 
-	local cellItems = world.getCellByName("toddtest"):getAll(types.Container)
-	cubeChest:teleport("toddtest", util.vector3(2035.569580078125, 3245.40771484375, -152.9093017578125))
+	-- local cellItems = world.getCellByName("toddtest"):getAll(types.Container)
+	-- cubeChest:teleport("toddtest", util.vector3(2035.569580078125, 3245.40771484375, -152.9093017578125))
 
 --allow the cubeChest time to teleport.
-	async:newUnsavableSimulationTimer(0.1, function() 
-		if startWithRecipes == true then
-			print("1book: " .. tostring(theBook))							--shows item exists
-			cubeChest = getCubeChest(data.player)
-			print("1cubechest: " .. tostring(cubeChest))					--shows item exists
-			theBook:moveInto(types.Container.inventory(cubeChest))			--shows item was deleted?!
-			print("2book: " .. tostring(theBook))
-		end
+	-- async:newUnsavableSimulationTimer(0.1, function()
+	-- 	if startWithRecipes == true then
+	-- 		print("1book: " .. tostring(theBook))							--shows item exists
+	-- 		cubeChest = getCubeChest(data.player)
+	-- 		print("1cubechest: " .. tostring(cubeChest))					--shows item exists
+	-- 		theBook:moveInto(types.Container.inventory(cubeChest))			--shows item was deleted?!
+	-- 		print("2book: " .. tostring(theBook))
+	-- 	end
 
-		--if we arent starting the player off with the cube, then at least shove one in a box for them
-		if startWithCube == true then
-			theCube:moveInto(types.Actor.inventory(data.player))
-		else
-			theCube:moveInto(types.Container.inventory(cubeChest))			
-		end
-	end)
+	-- 	--if we arent starting the player off with the cube, then at least shove one in a box for them
+	-- 	if startWithCube == true then
+	-- 	else
+	-- 		theCube:moveInto(types.Container.inventory(cubeChest))
+	-- 	end
+	-- end)
 
-	async:newUnsavableSimulationTimer(0.15, function()
-		--dirty hack to get around this https://gitlab.com/OpenMW/openmw/-/issues/7663
-		theBook = types.Container.inventory(cubeChest):getAll(types.Book)[1]
-	end)
+	-- async:newUnsavableSimulationTimer(0.15, function()
+		-- -- dirty hack to get around this https://gitlab.com/OpenMW/openmw/-/issues/7663
+	-- 	theBook = types.Container.inventory(cubeChest):getAll(types.Book)[1]
+	-- end)
 end
 
 local function onSave()
-	for _x, player in ipairs(world.players) do
+	for _, player in ipairs(world.players) do
 		modStorage:set("ownedKeys", ownedKeys)
 	end
 end
@@ -404,11 +403,11 @@ return {
 		getCubeContents = getCubeContents,
 		checkUserRecipes = checkUserRecipes,
 		checkModRecipes = checkModRecipes,
-		transmutecube = transmutecube
+		transmutecube = transmuteCube
 	},
-	engineHandlers = { 
+	engineHandlers = {
 		onInit = onInit,
-		onPlayerAdded = onPlayerAdded 
+		onPlayerAdded = onPlayerAdded
 	},
 	eventHandlers = {
 		event_openCube = event_openCube,
